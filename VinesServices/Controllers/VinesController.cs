b07446126f5cde36274ae84ca26274f22587bea1
d.georgiev.91@gmail.com <d.georgiev.91@gmail.com>
2013-08-29 19:07:18 +0300

    added sample idea

--- VinesServices/Controllers/VinesController.cs
+++ VinesServices/Controllers/VinesController.cs
@@ -3,11 +3,30 @@
     using System;
     using System.Linq;
     using System.Web.Http;
+    using VinesServices.Models;
 
     public class VinesController : ApiController
     {
         // GET api/albums
         public IQueryable<VinesList> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/albums
+        public VineFull Get(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/albums
+        public VineFull Search(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/albums
+        public VineFull GetRandom()
         {
             throw new NotImplementedException();
         }
