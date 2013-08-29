a94908058b9e1491c2f42436e3210dc2675ceb3e
d.georgiev.91@gmail.com <d.georgiev.91@gmail.com>
2013-08-29 18:15:38 +0300

    Initial Commit

---
+++ VinesServices/Controllers/VinesController.cs
@@ -0,0 +1,15 @@
+﻿namespace VinesServices.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Http;
+
+    public class VinesController : ApiController
+    {
+        // GET api/albums
+        public IQueryable<VinesList> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VinesServices/Models/Vine.cs
@@ -0,0 +1,30 @@
+﻿namespace VinesServices.Models
+{
+    using HtmlAgilityPack;
+
+    public class Vine
+    {
+        public string Title { get; set; }
+
+        public string VineUrl { get; set; }
+
+        public string VinePosterUrl { get; set; }
+
+        public string Author { get; set; }
+
+        public static Vine Parse(string html)
+        {
+            var vine = new Vine();
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            var articleNode = htmlDocument.DocumentNode.SelectSingleNode("article");
+            vine.VineUrl = articleNode.SelectSingleNode("a").Attributes["href"].Value;
+            vine.Title = articleNode.SelectSingleNode("a/header/h3").InnerText;
+            vine.VinePosterUrl = articleNode.SelectSingleNode("a/div/img").Attributes["src"].Value;
+            vine.Author = articleNode.SelectSingleNode("a/footer/p").InnerText;
+
+            return vine;
+        }
+    }
+}
